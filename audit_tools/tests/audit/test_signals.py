b317ef4fc09182889f919cdf0677a2db47c8cffd
PeRDy <perdy.hh@gmail.com>
2016-01-09 02:57:16 +0100

    Tests for signals module

--- audit_tools/tests/audit/test_signals.py
+++ audit_tools/tests/audit/test_signals.py
@@ -90,6 +90,13 @@ class SignalsTestCase(TestCase):
         self.assertEqual(content['old']['float_field'], 1.0)
         self.assertItemsEqual(content['changes'].keys(), keys)
 
+    def test_content_empty(self):
+        content = signals._extract_content_data()
+
+        self.assertIsNone(content['old'])
+        self.assertIsNone(content['new'])
+        self.assertEqual(content['changes'], {})
+
     def test_instance_data(self):
         model = TestModel()
         model.id = 1
@@ -148,23 +155,24 @@ class SignalsTestCase(TestCase):
             float_field=1.0,
         )
         old_object = serialize_model_instance(model)
-        model.string_field = 'Modified'
-        model.integer_field = 2
-        model.float_field = 2.0
-        new_object = serialize_model_instance(model)
-        content = signals._extract_content_data(old_object, new_object)
-        extract_content_data.return_value = content
+        with patch.dict('audit_tools.audit.signals._CACHE', {id(model): old_object}):
+            model.string_field = 'Modified'
+            model.integer_field = 2
+            model.float_field = 2.0
+            new_object = serialize_model_instance(model)
+            content = signals._extract_content_data(old_object, new_object)
+            extract_content_data.return_value = content
 
-        signals._post_save(model, instance=model)
+            signals._post_save(model, instance=model)
 
-        # Check that extract process and access was done successfully
-        self.assertEqual(cache.get_last_access.call_count, 1)
-        self.assertEqual(cache.get_process.call_count, 1)
-        self.assertEqual(extract_process_data.call_count, 1)
+            # Check that extract process and access was done successfully
+            self.assertEqual(cache.get_last_access.call_count, 1)
+            self.assertEqual(cache.get_process.call_count, 1)
+            self.assertEqual(extract_process_data.call_count, 1)
 
-        # Check that was called synchronously
-        self.assertEqual(save_model_action.call_count, 1)
-        self.assertEqual(save_model_action.apply_async.call_count, 0)
+            # Check that was called synchronously
+            self.assertEqual(save_model_action.call_count, 1)
+            self.assertEqual(save_model_action.apply_async.call_count, 0)
 
     @patch('audit_tools.audit.signals.extract_process_data')
     @patch('audit_tools.audit.signals.cache')
@@ -173,6 +181,39 @@ class SignalsTestCase(TestCase):
     @patch('audit_tools.audit.signals.settings')
     def test_post_save_async(self, settings, save_model_action, extract_content_data, cache, extract_process_data):
         settings.RUN_ASYNC = True
+        model = TestModel(
+            string_field='Test',
+            integer_field=1,
+            float_field=1.0,
+        )
+        old_object = serialize_model_instance(model)
+        with patch.dict('audit_tools.audit.signals._CACHE', {id(model): old_object}):
+            model.string_field = 'Modified'
+            model.integer_field = 2
+            model.float_field = 2.0
+            new_object = serialize_model_instance(model)
+            content = signals._extract_content_data(old_object, new_object)
+            extract_content_data.return_value = content
+
+            signals._post_save(model, instance=model)
+
+            # Check that extract process and access was done successfully
+            self.assertEqual(cache.get_last_access.call_count, 1)
+            self.assertEqual(cache.get_process.call_count, 1)
+            self.assertEqual(extract_process_data.call_count, 1)
+
+            # Check that was called asynchronously
+            self.assertEqual(save_model_action.call_count, 0)
+            self.assertEqual(save_model_action.apply_async.call_count, 1)
+
+    @patch('audit_tools.audit.signals.extract_process_data')
+    @patch('audit_tools.audit.signals.cache')
+    @patch('audit_tools.audit.signals._extract_content_data')
+    @patch('audit_tools.audit.tasks.save_model_action')
+    @patch('audit_tools.audit.signals.settings')
+    def test_post_save_not_old_data(self, settings, save_model_action, extract_content_data, cache,
+                                    extract_process_data):
+        settings.RUN_ASYNC = False
         model = TestModel(
             string_field='Test',
             integer_field=1,
@@ -193,9 +234,55 @@ class SignalsTestCase(TestCase):
         self.assertEqual(cache.get_process.call_count, 1)
         self.assertEqual(extract_process_data.call_count, 1)
 
-        # Check that was called asynchronously
-        self.assertEqual(save_model_action.call_count, 0)
-        self.assertEqual(save_model_action.apply_async.call_count, 1)
+        # Check that was called synchronously
+        self.assertEqual(save_model_action.call_count, 1)
+        self.assertEqual(save_model_action.apply_async.call_count, 0)
+
+    @patch('audit_tools.audit.signals.logger')
+    @patch('audit_tools.audit.signals.extract_process_data')
+    @patch('audit_tools.audit.signals.cache')
+    @patch('audit_tools.audit.signals._extract_content_data')
+    @patch('audit_tools.audit.tasks.save_model_action')
+    @patch('audit_tools.audit.signals.settings')
+    def test_post_save_fail_save_model_action(self, settings, save_model_action, extract_content_data, cache,
+                                              extract_process_data, logger):
+        settings.RUN_ASYNC = False
+        save_model_action.side_effect = Exception
+        model = TestModel(
+            string_field='Test',
+            integer_field=1,
+            float_field=1.0,
+        )
+        old_object = serialize_model_instance(model)
+        model.string_field = 'Modified'
+        model.integer_field = 2
+        model.float_field = 2.0
+        new_object = serialize_model_instance(model)
+        content = signals._extract_content_data(old_object, new_object)
+        extract_content_data.return_value = content
+
+        signals._post_save(model, instance=model)
+
+        # Check that extract process and access was done successfully
+        self.assertEqual(cache.get_last_access.call_count, 1)
+        self.assertEqual(cache.get_process.call_count, 1)
+        self.assertEqual(extract_process_data.call_count, 1)
+
+        # Check that save process wasn't done successfully
+        self.assertEqual(logger.exception.call_count, 1)
+
+    @patch('audit_tools.audit.signals.serialize_model_instance', side_effect=Exception)
+    @patch('audit_tools.audit.signals.logger')
+    def test_post_save_fail(self, logger, serialize_model_instance_mock):
+        model = TestModel(
+                string_field='Test',
+                integer_field=1,
+                float_field=1.0,
+        )
+        signals._post_save(model, instance=model)
+
+        # Check that save process wasn't done successfully
+        self.assertEqual(logger.exception.call_count, 1)
 
     @patch('audit_tools.audit.signals.extract_process_data')
     @patch('audit_tools.audit.signals.cache')
@@ -260,6 +347,47 @@ class SignalsTestCase(TestCase):
         # Check that was called asynchronously
         self.assertEqual(save_model_action.call_count, 0)
         self.assertEqual(save_model_action.apply_async.call_count, 1)
+
+    @patch('audit_tools.audit.signals.logger')
+    @patch('audit_tools.audit.signals.extract_process_data')
+    @patch('audit_tools.audit.signals.cache')
+    @patch('audit_tools.audit.signals._extract_content_data')
+    @patch('audit_tools.audit.tasks.save_model_action', side_effect=Exception)
+    @patch('audit_tools.audit.signals.settings')
+    def test_pre_delete_fail_save_model_action(self, settings, save_model_action, extract_content_data, cache,
+                                               extract_process_data, logger):
+        settings.RUN_ASYNC = False
+        model = TestModel(
+            string_field='Test',
+            integer_field=1,
+            float_field=1.0,
+        )
+        content = signals._extract_content_data()
+        extract_content_data.return_value = content
+
+        signals._pre_delete(model, instance=model)
+
+        # Check that extract process and access was done successfully
+        self.assertEqual(cache.get_last_access.call_count, 1)
+        self.assertEqual(cache.get_process.call_count, 1)
+        self.assertEqual(extract_process_data.call_count, 1)
+
+        # Check that save process wasn't done successfully
+        self.assertEqual(logger.exception.call_count, 1)
+
+    @patch('audit_tools.audit.signals.logger')
+    @patch('audit_tools.audit.signals.serialize_model_instance', side_effect=Exception)
+    def test_pre_delete_fail(self, serialize_model_instance_mock, logger):
+        model = TestModel(
+            string_field='Test',
+            integer_field=1,
+            float_field=1.0,
+        )
+
+        signals._pre_delete(model, instance=model)
+
+        # Check that save process wasn't done successfully
+        self.assertEqual(logger.exception.call_count, 1)
 
     @patch('audit_tools.audit.signals.dynamic_import', return_value=None)
     @patch('audit_tools.audit.signals.logger')
