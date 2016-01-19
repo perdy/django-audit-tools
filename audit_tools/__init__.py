d9fdf8780df01a9b9389042fd5ae4b139d4f6f55
PeRDy <perdy.hh@gmail.com>
2016-01-20 00:16:24 +0100

    Bump version to 0.4.0

--- audit_tools/__init__.py
+++ audit_tools/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 """django-audit-tools"""
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __license__ = 'GNU GPL v2'
 
 __author__ = 'José Antonio Perdiguero López'
